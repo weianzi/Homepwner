252f81c5dd9d12e7972204f18fa85c18d88a40d4
ancool <ancool@foxmail.com>
2015-12-30 01:08:08 +0800

    init

---
+++ BNRItemsViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRItemsViewController.h
+//  Homepwner
+//
+//  Created by an on 15/12/30.
+//  Copyright (c) 2015年 ancool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRItemsViewController : UITableViewController
+
+@end
---
+++ BNRItemsViewController.m
@@ -0,0 +1,24 @@
+//
+//  BNRItemsViewController.m
+//  Homepwner
+//
+//  Created by an on 15/12/30.
+//  Copyright (c) 2015年 ancool. All rights reserved.
+//
+
+#import "BNRItemsViewController.h"
+
+@implementation BNRItemsViewController
+
+-(instancetype)init
+{
+    self = [super initWithStyle:UITableViewStylePlain];
+    return self;
+}
+
+-(instancetype)initWithStyle:(UITableViewStyle)style
+{
+    return [self init];
+}
+
+@end
